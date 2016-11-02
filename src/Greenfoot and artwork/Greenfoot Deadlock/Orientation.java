e26411f2019a3a0f09f9c26386956af612cb0eca
tuanungquoc <tuanungquoc@gmail.com>
2016-11-02 14:40:33 -0700

    - Adding methods for Orientation class and DoublyLinkedList class
    - DoublyLinkedList will help to support for orientation config and
    checking neighbors from subclass of Orientation class.

--- src/Greenfoot and artwork/Greenfoot Deadlock/Orientation.java
+++ src/Greenfoot and artwork/Greenfoot Deadlock/Orientation.java
@@ -4,7 +4,10 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import greenfoot.*;
+import java.util.*;
 public interface Orientation  
 {
-    
+    void addPlayersToMatrix(ArrayList<Player> players);
+    boolean checkingNeighborhood(Player player1, Player player2);
 }
